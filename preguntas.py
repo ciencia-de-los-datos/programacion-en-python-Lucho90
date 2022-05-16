813f89fca864c8072b5497930144522f4355be0c
Luis <66659389+Lucho90@users.noreply.github.com>
2022-05-16 13:57:35 -0500

    Add files via upload

--- preguntas.py
+++ preguntas.py
@@ -11,6 +11,10 @@ Utilice el archivo `data.csv` para resolver las preguntas.
 
 
 """
+data = open("data.csv", "r").readlines()
+data = [row.replace("\t",",") for row in data]
+data = [row.replace("\n", "") for row in data]
+data = [row.split(",") for row in data]
 
 
 def pregunta_01():
@@ -21,7 +25,9 @@ def pregunta_01():
     214
 
     """
-    return
+    column=1
+    
+    return sum(int(row[column]) for row in data)
 
 
 def pregunta_02():
@@ -39,8 +45,21 @@ def pregunta_02():
     ]
 
     """
-    return
-
+    def countX(lst, x):
+      count = 0
+      for ele in lst:
+        if (ele == x):
+          count = count + 1
+      return count
+    lista_punto = [x[0] for x in data]  
+    list_set = set(lista_punto)
+    unique_list = (list(list_set))
+    letras = [x for x in unique_list]
+    result = []
+    for letra in letras:
+       resultado = countX( lista_punto ,letra)
+       result.append((letra,resultado ))
+    return print(sorted(result))
 
 def pregunta_03():
     """
@@ -57,7 +76,24 @@ def pregunta_03():
     ]
 
     """
-    return
+    lista_punto = [(x[0],x[1]) for x in data]   
+    lista_punto1 = [x[0] for x in lista_punto]  
+    list_set = set(lista_punto1)
+    unique_list = (list(list_set))
+
+    letras = [x for x in unique_list]
+
+
+    resultado = []
+    for letra in letras:
+      result= []
+      for element in lista_punto:
+        if element[0]==letra:
+          result.append(int(element[1]))
+          result2 = sum(result)
+          answer = (element[0],result2)
+      resultado.append(answer)  
+    return print(sorted(resultado))
 
 
 def pregunta_04():
@@ -82,7 +118,19 @@ def pregunta_04():
     ]
 
     """
-    return
+    dat = [row[2].split("-") for row in data]
+    #dat
+    months = ['01','02','03','04','05','06','07','08','09','10','11','12']
+    resultado=[]
+    for month in months:
+      result=[]
+      for x in dat:
+        if month == x[1]:
+          result.append(int(x[1]))
+          result2 = len(result)
+          answer = (x[1],result2)
+      resultado.append(answer)
+    return resultado
 
 
 def pregunta_05():
@@ -100,7 +148,24 @@ def pregunta_05():
     ]
 
     """
-    return
+    lista_punto = [(x[0],x[1]) for x in data] 
+    lista_punto1 = [x[0] for x in lista_punto]  
+    list_set = set(lista_punto1)
+    unique_list = (list(list_set))
+
+    letras = [x for x in unique_list]
+
+    resultado=[]
+    for letra in letras:
+      result=[]
+      for x in lista_punto:
+        if letra == x[0]:
+          result.append(int(x[1]))
+          result2 = min(result)
+          result3 = max(result)
+          answer = (x[0],result3,result2)
+      resultado.append(answer) 
+    return print(sorted(resultado))
 
 
 def pregunta_06():
@@ -125,7 +190,71 @@ def pregunta_06():
     ]
 
     """
-    return
+    parte_1 =[row[0:3] for row in data]
+    parte_1[0:2]
+
+    #Part_2- tiene las letras del medio
+    lista_letras = [[value for value in row if len(value) == 1] for row in data]
+    lista_letras = [row[2:] for row in lista_letras]
+    largo_maximo_letras =max([len(row) for row in lista_letras])
+
+    parte_2 = []
+    lista_temporal = []
+    for row in lista_letras:
+      for i in range(len(row)):
+        lista_temporal.append(row[i])
+      while len(lista_temporal) < largo_maximo_letras:
+        lista_temporal.append('')
+      parte_2.append(lista_temporal)
+      lista_temporal = []
+    parte_2[0:2]
+
+    #Part_3 - Contiene los diccionarios
+    lista_diccionario = [[value for value in row if len(value) >=5] for row in data]
+    lista_diccionario = [row[1:] for row in lista_diccionario]
+
+    diccionario = {}
+    parte_3 = []
+    for row in lista_diccionario:
+      for i in range(len(row)):
+        a,b =row[i].split(":")
+        diccionario[a] = int(b)
+      parte_3.append(diccionario)
+      diccionario = {}
+    parte_3[0:2]
+
+    list(zip(parte_1,parte_2,parte_3))[0]
+
+    datad = []
+    lista_intermedia = []
+    lista_temporal = []
+
+    for row in list(zip(parte_1,parte_2)):
+      for element in row:
+        for value in element:
+          lista_intermedia.append(value)
+      datad.append(lista_intermedia)
+      lista_intermedia = []
+
+    for i in range(len(datad)):
+      datad[i].append(parte_3[i])
+
+    datad[0:3]
+
+
+    resultado = []
+    llaves_unicas =set()
+    for row in [list(row[7].keys()) for row in datad]:
+      for element in row:
+        llaves_unicas.add(element)
+    llaves_unicas
+
+    for value in llaves_unicas:
+      resultado.append((value,min([row[7][value] for row in datad if value in list(row[7].keys())]),max([row[7][value] for row in datad if value in list(row[7].keys())])))
+    resultado
+
+    resultado = sorted(resultado, key=lambda valor : valor[0])
+    return resultado
 
 
 def pregunta_07():
@@ -149,7 +278,21 @@ def pregunta_07():
     ]
 
     """
-    return
+    columna1_2=[[x[1], x[0]] for x in data]
+    columna1=sorted(set([x[1] for x in data])) # Aqui se indica que se organice en orden descendente los numero y que solo imprima un numero, y no de forma repetida.
+    resultfinal=[]# Se crea un diccionario para poder almacenar los datos. 
+
+    #En el siguiente fragmento de código se itera en la columna1 y dos, en la cual se le indica que imprima por cada numero las letras que este contiene. 
+
+    for k in columna1:
+      n=[]
+      for f in columna1_2:
+        if k == f[0]:
+          n=n+[f[1]]
+   
+      result1=(k,n)
+      a = print(result1)
+    return a
 
 
 def pregunta_08():
@@ -174,7 +317,21 @@ def pregunta_08():
     ]
 
     """
-    return
+    columnajuntas=[[x[1], x[0]] for x in data]
+    columnauna=sorted(set([x[1] for x in data])) 
+    for k in columnauna:
+      n=[]
+      for f in columnajuntas:
+        if k == f[0]:
+          n=n+[f[1]]
+          result1=n
+          resultanlist= []
+          
+          for element in result1:
+            if element not in resultanlist:
+              resultanlist.append(element)
+      b= print((k,resultanlist))
+    return b
 
 
 def pregunta_09():
@@ -197,7 +354,44 @@ def pregunta_09():
     }
 
     """
-    return
+    colum5_1=[x[5:]  for x in data] 
+
+    columnaA=[]
+    for i in colum5_1:
+      row = []
+      for x in i:
+        if len(x)>=5:
+          row.append(x)
+
+      columnaA.append(row)
+  
+    columnaB=[]
+    for i in columnaA:
+      for a in i:
+        columnaB.append(a)
+    columnaB=sorted(columnaB)
+
+    columnaC=[]
+    columnaD=[x.replace(":",",") for x in columnaB] 
+    columnaD=[x.split(",") for x in columnaD]
+    columnaE=set([x[0] for x in columnaD])
+    resultfinal=[]
+
+    for k in columnaE:
+      n=[]
+      for f in columnaD:
+        if k == f[0]:
+          n=n+[f[1]]
+      result1=[k,len(n)]
+  
+      resultfinal=resultfinal+[result1]
+  
+    resultfinal = sorted(resultfinal)
+
+    for valor in resultfinal:
+      columf= valor[0]+","+str(valor[1])
+      c=print(columf)
+    return c
 
 
 def pregunta_10():
@@ -218,7 +412,19 @@ def pregunta_10():
 
 
     """
-    return
+    import csv
+
+    colum0 = [line[0] for line in data]
+    colum3 = [line[3:] for line in data]
+    datosa= [[fila for fila in line if len(fila) == 1] for line in colum3]
+    datosb = [[fila for fila in line if len(fila) >4] for line in colum3]
+    datosc = [[colum0[i], len(datosa[i]), len(datosb[i])] for i in range(len(colum3))]
+ 
+    with open( "y.csv", 'w', newline='') as y_file:
+         writer = csv.writer(y_file, quoting=csv.QUOTE_NONE,delimiter=',')
+         writer.writerows(datosc)
+    d = print(open("y.csv", "r").read())
+    return d
 
 
 def pregunta_11():
@@ -239,7 +445,28 @@ def pregunta_11():
 
 
     """
-    return
+    colum2 = [[fila for fila in line if len(fila) == 1] for line in data]
+    colum2 = [line[1:] for line in colum2]
+    culum3 = [line[2:] for line in colum2]
+    lst = []
+    for valores in culum3:
+      for vaina in valores:
+        lst = lst+[vaina]
+    lst = set(list(lst))
+    lst=sorted(lst)
+    respuesta = []
+    for numerico in lst:
+      num = [numerico]
+      contador = 0
+      for fila in colum2:
+        if numerico in fila:
+          contador = contador + int(fila[0])
+      num = num + [contador]
+      respuesta = respuesta + [num]
+    for line in respuesta:
+      final = line[0]+","+str(line[1])
+      e=print(final)
+    return e
 
 
 def pregunta_12():
@@ -257,4 +484,12 @@ def pregunta_12():
     }
 
     """
-    return
+    import re
+
+    rows = [(line[0], str(line[5:])) for line in data]
+    arab = []
+    result = []
+    for i in rows:
+      f = print(i[0]+','+str(sum([int(j) for j in re.findall(r'-?\d+\.?\d*', i[1])])))
+    return f
+ 
